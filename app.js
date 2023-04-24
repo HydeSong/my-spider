b45fa3db05bb0d99044eed24a3df6ed258ef4f18
hyde <hyde.song@yahoo.com>
2023-04-24 10:28:22 +0800

    rss

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cheerio = require('cheerio')
 const fs = require('fs')
 const axios = require('axios')
 const rss = require('rss')
+const ora = require('ora')
 
 const app = express()
 const baseURL = 'https://www.mp4kan.com'
@@ -26,39 +27,40 @@ async function getMoviePageList() {
 // 爬取每个电影页面的磁力链接数据
 async function getMovies(moviePageList) {
     const movies = []
-    const url = []
 
     for (const movieUrl of moviePageList) {
         const response = await axios.get(movieUrl)
         const $ = cheerio.load(response.data)
 
         // 获取电影详情
-        const title = $('.content .article-header h1').text()
-        const poster = $('.content .article-header pic img').attr('src')
+        // const poster = $('.content .article-header pic img').attr('src')
         const description = $('.content .info p').text()
+        const date = new Date()
         // 获取磁力链接
         $('.down-list li .url-left a').each((index, element) => {
             const $element = $(element)
-            const href = $element.attr('href')
-            href && href.includes('magnet') && url.push(href)
-        })
-        movies.push({
-            title,
-            poster,
-            description,
-            url: url[0]
+            const title = $element.attr('title').replace('磁力链下载', '')
+            const url = $element.attr('href')
+            if (url && url.includes('magnet')) {
+                movies.push({
+                    title,
+                    // poster,
+                    description,
+                    url,
+                    date
+                })
+            }
         })
     }
 
-    console.log(JSON.stringify(movies))
     return movies
 }
 
 // 生成 RSS
 function generateRSS(items) {
     const feed = new rss({
-        title: 'My RSS Feed',
-        description: 'A description of my RSS feed',
+        title: 'Movie RSS Feed',
+        description: 'Get updates from www.mp4kan.com/custom/update.html',
         feed_url: 'https://example.com/rss.xml',
         site_url: 'https://example.com',
         language: 'en',
@@ -68,30 +70,34 @@ function generateRSS(items) {
     for (const item of items) {
         feed.item({
             title: item.title,
-            poster: item.poster,
             description: item.description,
-            url: item.url
+            url: item.url,
+            date: item.date,
         })
     }
 
-    // 生成 RSS 并写入文件
+    // 生成 RSS
     const xml = feed.xml()
     return xml
 }
 
 app.get('/rss', async (req, res, next) => {
     try {
-        console.log('开始拉取订阅源')
+        const spinner = ora('开始拉取订阅源').start();
         const moviePageList = await getMoviePageList()
-        console.log('成功获取最新更新电影列表')
+        spinner.color = 'green'
+        spinner.text = '成功获取最新更新电影列表 '
         const movies = await getMovies(moviePageList)
-        console.log('成功获取电影信息')
+        console.log('movies磁力链接总共有 ', movies.length)
+        spinner.text = '成功获取电影信息'
         const xml = generateRSS(movies)
-        console.log('成功生成rss')
         fs.writeFileSync('rss.xml', xml)
+        spinner.text = '成功生成rss'
         res.setHeader('Content-Type', 'text/xml; charset=UTF-8')
         res.send(xml)
-        console.log('success')
+        spinner.text = '成功发送rss'
+        spinner.stop()
+        console.log('成功发送rss')
     } catch (err) {
         console.log('error....', err)
     }
